fa46b268a97ddc9c584b9d6e9597b65dc100205d
USER <user@USERs-MacBook-Pro.local>
2026-02-21 03:33:34 -0500

    added use client to all pages

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { Hero } from './../../components/Hero';
 import { FeatureBlocks } from './../../components/FeatureBlocks';
 import { Testimonials } from './../../components/Testimonials';
--- app/TCFTEFPrep/page.tsx
+++ app/TCFTEFPrep/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { Card, CardContent, CardHeader } from './../../components/ui/card';
 import { Button } from './../../components/ui/button';
 import { CheckCircle, BookOpen, Headphones, FileText, Video } from 'lucide-react';
